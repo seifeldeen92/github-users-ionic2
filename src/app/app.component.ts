02069853136dba7c17f83f6795e507ed3b1e22ba
seifeldeen92 <s.khaled_92@hotmail.com>
2017-01-24 14:50:55 +0200

    Adding project files

---
+++ src/app/app.component.ts
@@ -0,0 +1,47 @@
+import { Component, ViewChild } from '@angular/core';
+
+import { Platform, MenuController, Nav } from 'ionic-angular';
+
+import { StatusBar, Splashscreen } from 'ionic-native';
+
+import { UsersPage } from '../pages/users/users';
+import { ReposPage } from '../pages/repos/repos';
+import { OrganisationsPage } from '../pages/organisations/organisations';
+
+
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  @ViewChild(Nav) nav: Nav;
+
+    // make UsersPage the root (or first) page
+    rootPage: any = UsersPage;
+    pages: Array<{title: string, component: any}>;
+
+    constructor(public platform: Platform,  public menu: MenuController) {
+      this.initializeApp();
+
+      // set our app's pages
+      this.pages = [
+        { title: 'Users', component: UsersPage },
+        { title: 'Repos', component: ReposPage },
+        { title: 'Organisations', component: OrganisationsPage },
+      ];
+    }
+
+    initializeApp() {
+      this.platform.ready().then(() => {
+        // Okay, so the platform is ready and our plugins are available.
+        // Here you can do any higher level native things you might need.
+        StatusBar.styleDefault();
+      });
+    }
+
+    openPage(page) {
+      // close the menu when clicking a link from the menu
+      this.menu.close();
+      // navigate to the new page if it is not the current page
+      this.nav.setRoot(page.component);
+    }
+  }
---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { IonicApp, IonicModule } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { UsersPage } from '../pages/users/users';
+import { ReposPage } from '../pages/repos/repos';
+import { OrganisationsPage } from '../pages/organisations/organisations';
+import {UserDetailsPage } from '../pages/user-details/user-details';
+
+
+import { GithubUsers } from '../providers/github-users';
+
+
+@NgModule({
+  declarations: [
+    MyApp,
+    UsersPage,
+    ReposPage,
+    OrganisationsPage,
+    UserDetailsPage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    UsersPage,
+    ReposPage,
+    OrganisationsPage,
+    UserDetailsPage
+  ],
+  providers: [GithubUsers]
+})
+export class AppModule {}
